90fec578c2c5d8a8b4e438c5776717b6170be471
tdorado <tdorado@itba.edu.ar>
2020-08-13 15:42:14 -0300

    Fix de brute force

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args){
         ParticlesGenerator particlesGenerator = new ParticlesGenerator(0.25, 20, 500);
         List<Particle> particles = particlesGenerator.getParticles();
-        BruteForceMethod bfm = new BruteForceMethod(particles, 5, 1.0, true);
+        BruteForceMethod bfm = new BruteForceMethod(particles, 20, 1.0, true);
         CellIndexMethod cim = new CellIndexMethod(particles, true, 1.0, 20, 10);
         FileWriter.writeInput(particles, "test", 20.0, 500);
-        FileWriter.writeOutput(particles, cim.getNeighbours(), "test_bfm", bfm.getTotalTime(), bfm.getInteractionRadius());
+        FileWriter.writeOutput(particles, bfm.getNeighbours(), "test_bfm", bfm.getTotalTime(), bfm.getInteractionRadius());
         FileWriter.writeOutput(particles, cim.getNeighbours(), "test_cim", cim.getTotalTime(), cim.getInteractionRadius());
     }
 }
